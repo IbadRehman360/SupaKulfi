1197aa29c05c117a839e6c8b147f923131faeb82
0xJordanx0 <ahmedjamlan12@gmail.com>
2023-09-08 01:43:44 +0400

    Fixed return data of usePrefilledData hook

--- src/hooks/usePrefilledData.jsx
+++ src/hooks/usePrefilledData.jsx
@@ -2,7 +2,7 @@ import { useNavigate } from "react-router-dom";
 import supabase from "../services/supabase";
 import { useEffect, useState } from "react";
 
-async function usePrefilledData(id) {
+function usePrefilledData(id) {
   const navigate = useNavigate();
   const [error, setError] = useState("");
   const [getOneKulfiData, setgetOneKulfiData] = useState([]);
@@ -20,8 +20,7 @@ async function usePrefilledData(id) {
         console.log(error);
       }
       if (data) {
-        console.log(setgetOneKulfiData(data));
-        console.log(data);
+        setgetOneKulfiData(data)
       }
     }
 
